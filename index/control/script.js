0b7afb771c8290a7d54e1c6080b53c8ed522eca9
wzz <13731160065@users.noreply.github.com>
2018-02-28 11:06:50 +0800

    修改为view方法

--- index/control/script.js
+++ index/control/script.js
@@ -5,7 +5,7 @@ function viewDidLoad() {
         var array = new Array();
         
         //创建要修改的数据
-        var tvc_ocabc = OCH5_createReplaceFunc("TestVC", "abc:b:c:d:e:f:", "ocbbc");
+        var tvc_ocabc = OCH5_createReplaceFunc("TestVC", "abc:b:c:d:e:f:", "ocabc");
         
         array[0] = tvc_ocabc;
         
@@ -25,19 +25,23 @@ function ocbbc() {
 }
 
 function ocabc() {
-    //self
-    var self = OCH5_getValue_context_value(och5_JSContext, "currentController");
-    alert(self);
-    //self.view
-    var self_view = OCH5_getValue_context_value(self, "view");
-    
-    //aView = [[UIView alloc] init];
-    var aView = OCH5_allocWithClass("UIView");
-    //[self.view addSubview:aView];
-    OCH5_runFunc_context_funcName_1arg(self_view, "addSubview:", aView);
-    //aView.frame;
-    var self_view_frame = OCH5_getValue_context_value(self_view, "frame");
-    OCH5_log(self_view_frame);
-    //aView.frame = CGRectMake(0, 0, 100, 100);
-//    OCH5_setValue_context_valueName_value(aView, "", "")
+    try {
+        //self
+        var self = OCH5_getValue_context_value(och5_JSContext, "currentController");
+        
+        //self.view
+        var self_view = OCH5_getValue_context_value(self, "view");
+        
+        //aView = [[UIView alloc] init];
+        var aView = OCH5_allocWithClass("UIView");
+        //[self.view addSubview:aView];
+        OCH5_runFunc_context_funcName_1arg(self_view, "addSubview:", aView);
+        //aView.frame;
+        var self_view_frame = OCH5_getValue_context_value(self_view, "frame");
+        OCH5_log(self_view_frame);
+        //aView.frame = CGRectMake(0, 0, 100, 100);
+        //    OCH5_setValue_context_valueName_value(aView, "", "")
+    } catch (exp) {
+        alert(exp);
+    }
 }
